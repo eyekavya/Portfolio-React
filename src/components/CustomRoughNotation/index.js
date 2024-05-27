7f4bf32269961963e3587c279447e60c450c10a1
eyekavya <kavyasrivastava7830@gmail.com>
2024-05-27 14:33:45 +0530

    styled about me section

--- src/components/CustomRoughNotation/index.js
+++ src/components/CustomRoughNotation/index.js
@@ -2,20 +2,34 @@ import React from "react";
 
 import { RoughNotation } from "react-rough-notation";
 
-function CustomRoughNotation({ text, showAnnotation, type = "underline" }) {
+function CustomRoughNotation({
+  type = "underline",
+  showAnnotation = false,
+  animationDuration = 700,
+  strokeWidth = 3,
+  animate = true,
+  text = "",
+  color = "#8ad2ff",
+  iterations = 2,
+  padding = 5,
+  order = "1",
+  brackets,
+}) {
   return (
-    <>
-      <div>
-        <RoughNotation
-          type={type}
-          show={showAnnotation}
-          animationDuration={500}
-          strokeWidth={3}
-        >
-          {text}
-        </RoughNotation>
-      </div>
-    </>
+    <RoughNotation
+      type={type}
+      show={showAnnotation}
+      animationDuration={animationDuration}
+      strokeWidth={strokeWidth}
+      animate={animate}
+      color={color}
+      iterations={iterations}
+      padding={padding}
+      order={order}
+      brackets={brackets}
+    >
+      {text}
+    </RoughNotation>
   );
 }
 
