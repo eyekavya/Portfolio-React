baf1f36f276ba3657be2a0208d6017f43df76447
eyekavya <kavyasrivastava7830@gmail.com>
2024-05-29 00:51:17 +0530

    modified heading style

--- src/components/SectionHeading/StyledSectionHeading.js
+++ src/components/SectionHeading/StyledSectionHeading.js
@@ -18,7 +18,7 @@ export const StyledSectionHeading = styled.section`
   .heading-text {
     color: #1c1b21;
     font-family: "Exo 2", sans-serif;
-    font-weight: 700;
+    font-weight: 800;
     font-style: normal;
     font-size: 2.4rem;
     text-align: center;
