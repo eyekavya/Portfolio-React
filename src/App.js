d4e63584ccb9f169f5753880af38e7990c35c575
eyekavya <kavyasrivastava7830@gmail.com>
2024-06-05 23:50:37 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import About from "./components/About";
 import Hero from "./components/Hero";
-import Nav from "./components/Nav";
 import Skill from "./components/Skill";
 
 function App() {
