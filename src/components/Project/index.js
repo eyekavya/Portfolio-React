3f15f608edc092de8aa24539b4bd846f5739c6ed
eyekavya <kavyasrivastava7830@gmail.com>
2024-06-13 17:37:43 +0530

    added project preview coponent

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import SectionHeading from "../SectionHeading";
 import { StyledProject } from "./StyledProject";
 
 function Project() {
+  const [projectIndex, setProjectIndex] = useState(0);
+
+  let projectArray = [
+    "/images/project-preview/cheesy-slice.svg",
+    "/images/project-preview/shinchan.svg",
+    "/images/project-preview/forecastify.svg",
+  ];
   return (
     <StyledProject>
       <div className="project wrapper">
         <SectionHeading headingText={"Projects"} />
         <div className="flex-row">
-          <div className="btn btn-prev"></div>
+          <div
+            className="btn btn-prev"
+            onClick={() =>
+              projectIndex === 0
+                ? setProjectIndex(2)
+                : setProjectIndex(projectIndex - 1)
+            }
+          ></div>
           <div className="project-container">
-            <img
-              src="/images/project-preview/cheesy-slice.svg"
-              alt="Projects Preview"
-            />
+            <img src={projectArray[projectIndex]} alt="Project Preview" />
           </div>
-          <div className="btn btn-next"></div>
+          <div
+            className="btn btn-next"
+            onClick={() =>
+              projectIndex < projectArray.length - 1
+                ? setProjectIndex(projectIndex + 1)
+                : setProjectIndex(0)
+            }
+          ></div>
         </div>
       </div>
     </StyledProject>
